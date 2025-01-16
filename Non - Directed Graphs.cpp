1e0df64d601f2edcc8c5a56633fa4b13ee7d9549
Angelos Kamariadis <150456060+kamariadis@users.noreply.github.com>
2025-01-16 17:51:15 +0200

    Update and rename nonDirectedGraphs.cpp to Non - Directed Graphs.cpp

--- Non - Directed Graphs.cpp
+++ Non - Directed Graphs.cpp
@@ -10,13 +10,10 @@ public:
     int size() {
         return adj.size();
     }
-
     void addEdge(int f, int t) {
-        // symmetrical
         adj[f].push_back(t);
         adj[t].push_back(f);
     }
-
     void print() {
         for (auto const & vertex:adj) {
             cout << endl << "[" << vertex.first << "]";
@@ -24,17 +21,11 @@ public:
                 cout << " -> " << adjV;
         }
     }
-
-    // return the neighbours
     vector<int> neighbours(const int & n) {
-        // αν το στοιχείο υπάρχει, έχει μέγεθος !=0, οπότε απλά το επιστρέφουμε
         if (adj[n].size()) return adj[n];
-
-        // αν το στοιχείο δεν υπάρχει, θα δημιουργηθεί, αλλά αυτό δε μας χρειάζεται.
         adj.erase(n);
         return {};
     }
-
 private:
     map<int,vector<int>> adj;
 };
@@ -49,27 +40,13 @@ int main() {
     sg.addEdge(4, 6);
     sg.print();
     cout<<endl<<"n of vertices = "<<sg.size();
-
     cout << "\nNeighbours of 1\n";
     for (auto nn:sg.neighbours(1))
         cout << nn << "->";
     cout << endl;
-
     cout << "\nNeighbours of 7\n";
     for (auto nn:sg.neighbours(7))
         cout << nn << "->";
     cout << endl;
-
     return 0;
 }
-
-//
-// NTUA ECE ProgTech 2022-2023
-//
-// Non-directed graphs
-//
-// Δείτε τα Run configurations στο αρχείο CMakeLists.txt
-// Προσοχή: σε κάθε αλλαγή που κάνετε στο CMakeLists.txt *πρέπει* να κάνετε "Reload changes" ή "Reload CMake project"
-//
-// Επιλέξτε: (menu)Run -> (option)Run... -> nonDirected
-//
