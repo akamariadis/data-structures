d262fdc07dc87ca42501bb8076ae798c39ee2023
Angelos Kamariadis <150456060+kamariadis@users.noreply.github.com>
2025-01-16 17:52:42 +0200

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+struct node {
+    int info;
+    node *next;
+};
+
+node* read () {
+    node *head = nullptr;
+    node *n;
+    int data;
+
+    while (scanf("%d", &data) == 1) {
+        n = new node;
+        n -> info = data;
+        n -> next = head;
+        head = n;
+    }
+    return head;
+}
+
+void print (node *p) {
+    while (p != nullptr) {
+        cout << p -> info;
+        p = p -> next;
+    }
+}
+
+int main () {
+    node *list = read();
+    print(list);
+    return 0;
+}
