3bf0aee42feafb98091e6743ab372e77749f8ae6
Angelos Kamariadis <150456060+kamariadis@users.noreply.github.com>
2025-01-21 20:13:44 +0200

    Update Gen Trees.cpp

--- Gen Trees.cpp
+++ Gen Trees.cpp
@@ -2,66 +2,66 @@
 #include <queue>
 using namespace std;
 
-struct Node {
+struct node {
     int key;
-    vector<Node *>children;
+    vector<node *>children;
 };
 
-Node *newNode(int newKey) {
-    Node *temp = new Node;
-    temp->key = newKey;
+node *newnode(int newkey) {
+    Node *temp = new node;
+    temp -> key = newkey;
     return temp;
 }
 
-int height(Node *r){
-    if (r==NULL) return -1;
-    int h=-1;
-    for (Node *child: r->children) h = max(h, height(child));
+int height(node *r){
+    if (r == NULL) return -1;
+    int h =- 1;
+    for (node *child: r -> children) h = max(h, height(child));
     return h+1;
 }
 
-Node * searchTree(Node *r, const int &k) {
+node * searchTree(node *r, const int &k) {
     if (r != nullptr) {
-        if (r->key == k) return r;
-        for (Node *child: r->children) {
-            Node *s = searchTree(child, k);
+        if (r -> key == k) return r;
+        for (node *child: r -> children) {
+            node *s = searchTree(child, k);
             if (s != nullptr) return s;
         }
     }
     return nullptr;
 }
 
-void LevelOrderTraversal(Node * root) {
-    if (root==NULL) return;
+void LevelOrderTraversal(node * root) {
+    if (root == NULL) return;
     queue<Node *> q;
     q.push(root);
     while (!q.empty()) {
         int n = q.size();
         while (n > 0) {
-            Node * p = q.front();
+            node * p = q.front();
             q.pop(); n--;
-            cout << p->key << " ";
-            for (int i=0; i<p->children.size(); i++)
-                q.push(p->children[i]);
+            cout << p -> key << " ";
+            for (int i = 0; i < p -> children.size(); i++)
+                q.push(p -> children[i]);
         }
         cout << endl;
     }
 }
 
 int main () {
-    Node *root = newNode(7);
-    (root->children).push_back(newNode(5));
-    (root->children).push_back(newNode(10));
-    (root->children[0]->children).push_back(newNode(3));
-    (root->children[0]->children).push_back(newNode(6));
-    (root->children[0]->children).push_back(newNode(9));
-    (root->children[0]->children[0]->children).push_back(newNode(2));
-    (root->children[1]->children).push_back(newNode(11));
-    (root->children[1]->children[0]->children).push_back(newNode(4));
+    node *root = newnode(7);
+    (root->children).push_back(newnode(5));
+    (root->children).push_back(newnode(10));
+    (root->children[0]->children).push_back(newnode(3));
+    (root->children[0]->children).push_back(newnode(6));
+    (root->children[0]->children).push_back(newnode(9));
+    (root->children[0]->children[0]->children).push_back(newnode(2));
+    (root->children[1]->children).push_back(newnode(11));
+    (root->children[1]->children[0]->children).push_back(newnode(4));
     cout << "Level order traversal\n";
     LevelOrderTraversal(root);
     cout << "h=" << height(root) << endl;
-    Node *S = searchTree(root,99);
+    node *S = searchTree(root,99);
     if (S != NULL)
         cout << "found\n";
     else
