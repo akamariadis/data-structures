522bfd345b40f6f3e2d25456f9dbeafce5a9d9c8
Angelos Kamariadis <150456060+kamariadis@users.noreply.github.com>
2025-04-05 15:41:37 +0300

    Update Non - Directed Graphs.cpp

--- Non - Directed Graphs.cpp
+++ Non - Directed Graphs.cpp
@@ -1,52 +1,49 @@
 #include <iostream>
 #include <map>
-
+#include <vector>
 using namespace std;
 
 class nonDirected {
 public:
-    nonDirected(): adj({}) {}
-
-    int size() {
+    nonDirected() = default;
+    int size() const {
         return adj.size();
     }
     void addEdge(int f, int t) {
         adj[f].push_back(t);
         adj[t].push_back(f);
     }
-    void print() {
-        for (auto const & vertex:adj) {
-            cout << endl << "[" << vertex.first << "]";
-            for (auto adjV:vertex.second)
-                cout << " -> " << adjV;
+    void print() const {
+        for (const auto& vertex : adj) {
+            cout << "\n[" << vertex.first << "]";
+            for (int neighbor : vertex.second)
+                cout << " -> " << neighbor;
         }
     }
-    vector<int> neighbours(const int & n) {
-        if (adj[n].size()) return adj[n];
-        adj.erase(n);
+    vector<int> neighbours(const int& n) const {
+        auto it = adj.find(n);
+        if (it != adj.end()) return it->second;
         return {};
     }
 private:
-    map<int,vector<int>> adj;
+    map<int, vector<int>> adj;
 };
 
 int main() {
     nonDirected sg;
-    sg.addEdge(1,2);
+    sg.addEdge(1, 2);
     sg.addEdge(1, 3);
     sg.addEdge(2, 3);
     sg.addEdge(3, 4);
     sg.addEdge(3, 5);
     sg.addEdge(4, 6);
     sg.print();
-    cout<<endl<<"n of vertices = "<<sg.size();
-    cout << "\nNeighbours of 1\n";
-    for (auto nn:sg.neighbours(1))
-        cout << nn << "->";
-    cout << endl;
-    cout << "\nNeighbours of 7\n";
-    for (auto nn:sg.neighbours(7))
-        cout << nn << "->";
-    cout << endl;
+    cout << sg.size();
+    for (int nn : sg.neighbours(1))
+        cout << nn << " -> ";
+    cout << "null\n";
+    for (int nn : sg.neighbours(7))
+        cout << nn << " -> ";
+    cout << "null\n";
     return 0;
 }
